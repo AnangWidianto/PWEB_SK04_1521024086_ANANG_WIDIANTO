644277aee53a7e9c236ecbf25b35de6ce8cec849
AnangWidianto <anangw807@gmail.com>
2016-10-12 19:45:42 +0700

    Study Kasus 04
    Dalam study kasus ini ada 3 soal
    1.SK04_1
    2.SK04_2
    3.SK04_3

---
+++ 4_2_1521024086_ANANG WIDIANTO.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+<head>
+     <title> AKN Bojonegoro </title>
+</head>
+<body>
+     <?php
+         $KODEBARANG = "MB25";
+         switch ($KODEBARANG) {
+	     case "MB25";
+	         echo "anda sedang membeli motherboard dengan harga 250.000";
+	     break;
+	     case "PR80";
+	         echo "anda sedang membeli printer dengan harga 750.000";
+	     break;
+	     case "M717";
+	         echo "anda sedang membeli mouse dengan harga 30.000";
+	     break;
+	     case "MM34";
+	         echo "anda sedang membeli memori dengan harga 150.000";
+	     break;
+	     case "K987";
+	         echo "anda sedang membeli keyboard dengan harga 35.000";
+	     break;
+         }
+     ?>
+</body>
+</html>
